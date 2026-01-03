65a20ac9037c9db5b3c64484c266f8416a86956c
denostta <denostaanthony08@gmail.com>
2026-01-03 23:51:23 +0100

    created the header of Home page

--- frontend/food-del-app/src/App.jsx
+++ frontend/food-del-app/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/order" element={<PlaceOrder />} />
       </Routes>
     </div>
   );
